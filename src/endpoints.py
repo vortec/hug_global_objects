7cfcd7569a0f938fcd6c1ab73924053515e04d69
Fabian Kochem <fkochem@gmail.com>
2016-04-05 20:23:10 +0200

    Add comment for endpoints.py

--- src/endpoints.py
+++ src/endpoints.py
@@ -1,6 +1,12 @@
 import directives
 import hug
 
+
+# Endpoints use the directive and the io object directly, so that
+# unit testing those things will be a lot easier.
+# This way you can just pass your testing connection or a mock into
+# the endpoint.
+
 @hug.get('increase')
 def increase(redis: directives.redis, amount: hug.types.number=1):
     redis.incr('counter', amount)
