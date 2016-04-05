dfbaea66339d1e43ee2d071ac90e2a92134c0d32
Fabian Kochem <fkochem@gmail.com>
2016-04-05 20:23:38 +0200

    Fix typo

--- src/endpoints.py
+++ src/endpoints.py
@@ -2,7 +2,7 @@ import directives
 import hug
 
 
-# Endpoints use the directive and the io object directly, so that
+# Endpoints use the directive and not the io object directly, so that
 # unit testing those things will be a lot easier.
 # This way you can just pass your testing connection or a mock into
 # the endpoint.
